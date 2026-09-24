9e3771a2ebc9513dea917fe690eb7b8e3e71f0b5
Blue-Chief <adjemrughwev@gmail.com>
2026-09-24 15:42:34 +0100

    add login route for users

--- app/main.py
+++ app/main.py
@@ -5,11 +5,13 @@ from pydantic import BaseModel, Field
 from app.routers import tasks
 from app.routers import users
 from app.routers import projects
+from app.routers import auth
 
 app = FastAPI()
 app.include_router(tasks.router)
 app.include_router(users.router)
 app.include_router(projects.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def home():
