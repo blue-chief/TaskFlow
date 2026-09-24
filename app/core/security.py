420d2527d1f00ec7c2c20b3c386f367a8ecdde24
Blue-Chief <adjemrughwev@gmail.com>
2026-09-24 13:58:04 +0100

    refactored password hasher from bcrypt to pwdlib[argon2]

--- app/core/security.py
+++ app/core/security.py
@@ -1,12 +1,12 @@
-from passlib.context import CryptContext
+from pwdlib import PasswordHash
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+password_hash = PasswordHash.recommended()
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    return password_hash.hash(password)
 
 def verify_password(plain:str, hashed:str) -> bool:
-    return pwd_context.verify(plain, hashed)
+    return password_hash.verify(plain, hashed)
 
 def get_current_user() -> dict:
     return {"id":1, "username":"temp_user"}
